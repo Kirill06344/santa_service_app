afc80d2e183c667b232e76b6ec32e72ac8d53d00
dasu <kirill12093@mail.ru>
2023-01-05 01:44:56 +0300

    fix this

--- src/actions.rs
+++ src/actions.rs
@@ -14,11 +14,12 @@ pub fn add_group_with_name(conn: &mut PgConnection, gr_name: &String, u_id: i32)
     if res.is_err() {
         return Err(diesel::NotFound);
     }
-    use crate::schema::user_group::dsl::*;
-    insert_into(user_group).values((user_id.eq(u_id), group_id.eq(res.as_ref().unwrap().id))).execute(conn).expect("qwerty");
 
-    //use crate::schema::admins::dsl::*;
-    //insert_into(admins).values((user_id.eq(u_id), group_id.eq(res.unwrap().id))).execute(conn).expect("qwerty");
+    use crate::schema::user_group;
+    insert_into(user_group::table).values((user_group::user_id.eq(u_id), user_group::group_id.eq(res.as_ref().unwrap().id))).execute(conn).expect("qwerty");
+
+    use crate::schema::admins;
+    insert_into(admins::table).values((admins::user_id.eq(u_id),admins::group_id.eq(res.as_ref().unwrap().id))).execute(conn).expect("qwerty");
 
     res
 }
