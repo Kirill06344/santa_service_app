173d3055b3b7d522156fc740868cd60301fea7b4
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-01-05 07:19:28 +0300

    Fixing extra mutable

--- src/console.rs
+++ src/console.rs
@@ -1,7 +1,7 @@
 use std::{io};
 
 pub fn get_data(data: &mut String) -> Result<usize, &str> {
-    let mut current_length = io::stdin().read_line(data).expect("Error: Input is invalid");
+    let current_length = io::stdin().read_line(data).expect("Error: Input is invalid");
     if current_length == 0 {
         return Err("Programm was shutting down.");
     }
