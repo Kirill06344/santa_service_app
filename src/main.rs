2cdbc02f1b49115122262fc2a75ec8db057f6b93
dasu <kirill12093@mail.ru>
2023-01-07 01:07:26 +0300

    add login_input

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,12 @@ async fn main() -> Result<(), reqwest::Error> {
     loop {
         let mut user_login = String::new();
         println!("Enter your login:");
-        get_data(&mut user_login).unwrap();
         if get_data(&mut user_login).is_err() {
             break;
         }
+
+        let user_login = user_login.trim();
+
 
         let current_user_response = reqwest::Client::new()
             .post("http://127.0.0.1:8080/get_login_id")
@@ -56,7 +58,7 @@ async fn main() -> Result<(), reqwest::Error> {
             if get_data(&mut command_args).is_err() {
                 break;
             }
-            let command_args: Vec<_> = command_args.split(" ").collect();
+            let command_args: Vec<_> = command_args.trim().split(" ").collect();
             let command: &str = command_args[0];
             let amount_of_args: usize = command_args.len();
             if (amount_of_args == 3 && command != "assign")  || amount_of_args != 2 {
@@ -72,48 +74,48 @@ async fn main() -> Result<(), reqwest::Error> {
                 group_name: (command_args[1].to_string()),
                 admin_login
             };
-            match urls_container.get(command) {
-                Some(url) => {
-                    //1-st bariant
-                    let command_response = reqwest::Client::new()
-                        .post(*url)
-                        .json(&current_command)
-                        .send()
-                        .await?;
-                    
-                    if command_response.status() == StatusCode::OK {
-                        match command {
-                            "assign" => printAssignAdminResult(command_response.text()),
-                            "create" => printCreateGroupResult(command_response.text()),
-                            "delete" => printDeleteGroupResult(command_response.text()),
-                            "join" => printJoinGroupResult(command_response.text()),
-                            "leave" => printLeaveGroupResult(command_response.text()),
-                            "deleteAdmin" => printDeleteAdminResult(command_response.text()),
-                            "generateSantas" => printGenerateSantasResul(command_response.text()),
-                        }
-                    }
-                    else {
-                        let error_message: String = current_user_response.json().await?;
-                        println!("{}", error_message);
-                        continue;
-                    }
-
-                    //2-nd bariant
-                    // tokio::task::spawn_blocking(|| {
-                    //     match do_post_request(*url, current_command) {
-                    //         Ok(response) => {
-                    //             println!("{:?}", response);
-                    //         },
-                    //         Err(error) => {
-                    //             println!("Error: {:?}", error);
-                    //         }
-                    //     };
-                    // })
-                    // .await
-                    // .expect("Task panicked")
-                },
-                None => continue,
-            } 
+            // match urls_container.get(command) {
+            //     Some(url) => {
+            //         //1-st bariant
+            //         let command_response = reqwest::Client::new()
+            //             .post(*url)
+            //             .json(&current_command)
+            //             .send()
+            //             .await?;
+            //
+            //         if command_response.status() == StatusCode::OK {
+            //             match command {
+            //                 "assign" => printAssignAdminResult(command_response.text()),
+            //                 "create" => printCreateGroupResult(command_response.text()),
+            //                 "delete" => printDeleteGroupResult(command_response.text()),
+            //                 "join" => printJoinGroupResult(command_response.text()),
+            //                 "leave" => printLeaveGroupResult(command_response.text()),
+            //                 "deleteAdmin" => printDeleteAdminResult(command_response.text()),
+            //                 "generateSantas" => printGenerateSantasResul(command_response.text()),
+            //             }
+            //         }
+            //         else {
+            //             let error_message: String = current_user_response.json().await?;
+            //             println!("{}", error_message);
+            //             continue;
+            //         }
+            //
+            //         //2-nd bariant
+            //         // tokio::task::spawn_blocking(|| {
+            //         //     match do_post_request(*url, current_command) {
+            //         //         Ok(response) => {
+            //         //             println!("{:?}", response);
+            //         //         },
+            //         //         Err(error) => {
+            //         //             println!("Error: {:?}", error);
+            //         //         }
+            //         //     };
+            //         // })
+            //         // .await
+            //         // .expect("Task panicked")
+            //     },
+            //     None => continue,
+            // }
         }
 
         println!("You have been log out of profile. Would you like to log in to your profile?[Y/N]");
