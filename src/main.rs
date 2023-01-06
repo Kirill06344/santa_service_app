306e0c6f1a638571b2b6787a7fc3f8d2fde24a2a
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-01-06 06:54:28 +0300

    total fix: 1. changing structure of json with command in post request to server, 2. adding request to BD for learning current user-Id 3. adding new paths for commands

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,78 @@
 pub mod console;
 use console::get_data;
 pub mod structures;
-use structures::{Commands, User, a, zxc};
+use structures::{User, PersonalId};
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn do_post_request(url: &str, data: User) -> Result<(), reqwest::Error> {
+    let response: User = reqwest::Client::new()
+        .post(url)
+        .json(&data)
+        .send()
+        .await?
+        .json()
+        .await?;//Todo : do error handling
+    
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
+    let mut urls_container:HashMap<&str, &str> = HashMap::new();
+    urls_container.insert("create", "http://127.0.0.1:8080/users/addGroup");
+    urls_container.insert("delete", "http://127.0.0.1:8080/users/deleteGroup");
+    urls_container.insert("join", "http://127.0.0.1:8080/users/joinGroup");
+    urls_container.insert("leave", "http://127.0.0.1:8080/users/leaveGroup");
+    urls_container.insert("assign", "http://127.0.0.1:8080/users/assignAdmin");
+    urls_container.insert("deleteAdmin", "http://127.0.0.1:8080/users/deleteAdmin");
+    urls_container.insert("generateSantas", "http://127.0.0.1:8080/users/generateSanta");
+
+    let mut user_login = String::new();
+    println!("Enter your login:");
+    get_data(&mut user_login).unwrap(); //Todo : do error handling
+    // if get_data(&mut user_login).is_err() {
+    //     break;
+    // }
+    let current_login: PersonalId = PersonalId {
+        id: None,
+        login: user_login.to_string(),
+    };
+    let current_user: PersonalId = reqwest::Client::new()
+        .post("http://127.0.0.1:8080/userByLogin")
+        .json(&current_login)
+        .send()
+        .await?
+        .json()
+        .await?;//Todo : do error handling
+    
+    let current_id = current_user.id.unwrap();
+    
     loop {
-        let mut user_login = String::new();
         let mut command_args = String::new();
-        println!("Enter your login:");
-        if get_data(&mut user_login).is_err() {
-            break;
-        }
         println!("Enter your command:");
         if get_data(&mut command_args).is_err() {
             break;
         }
         let command_args: Vec<_> = command_args.split(" ").collect();
-        let command_type: &str = command_args[0];
-        let mut command: Option<Commands> = None;
-        let mut argument_option: Option<String> = None;
+        let command: &str = command_args[0];
         let amount_of_args: usize = command_args.len();
-        if (amount_of_args == 1 && command_type != "deleteAdmin") || amount_of_args > 2 {
+        if (amount_of_args != 3 && command == "assign")  || (amount_of_args != 2 && command != "assign") {
             continue;
         }
-        if amount_of_args == 2 {
-            argument_option.replace(command_args[1].to_string());
+        let mut admin_login: Option<String> = None;
+        if command == "assign" {
+            admin_login = Some(command_args[2].to_string());
         }
-        let argument: String = argument_option.unwrap();
-        match command_type {
-            "create" => command.replace(Commands::CreateGroup(argument)),
-            "delete" => command.replace(Commands::DeleteGroup(argument)),
-            "join" => command.replace(Commands::JoinGroup(argument)),
-            "leave" => command.replace(Commands::LeaveGroup(argument)),
-            "assign" => command.replace(Commands::AssignAdmin(argument)),
-            "deleteAdmin" => command.replace(Commands::DeleteAdmin),
-            "generateSantas" => command.replace(Commands::GenerateSantas(argument)),
-            _ => continue,
+        let current_command: User = User {
+            id: (current_id),
+            group_name: (command_args[1].to_string()),
+            admin_login
         };
-        let command: Commands = command.unwrap();
-        let user = User {
-            login: user_login,
-            command,
-        };
-
-
-        //print!("{}",serde_json::to_string(&user).unwrap());
-        //let a: str;
-        let d = a {
-            name: "zxcnmnqewfdds".to_string(),
-            user_id: 45
-        };
-        let response: zxc = reqwest::Client::new()
-            .post("http://127.0.0.1:8080/users/add_group")
-            .json(&d)
-            .send()
-            .await?
-            .json()
-            .await?;
-        //println!("{:#?}", response); // response from server
-        println!("{}, {}, {}", response.id, response.name, response.closed);
+        match urls_container.get(command) {
+            Some(url) => do_post_request(url, current_command)?,
+            None => continue,
+        } 
     }
     Ok(())
 }
--- src/structures.rs
+++ src/structures.rs
@@ -1,32 +1,14 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub enum Commands {
-    CreateGroup(String), // group name
-    DeleteGroup(String), // group name
-    JoinGroup(String), // group name
-    LeaveGroup(String), // group name
-    AssignAdmin(String), // user name
-    DeleteAdmin,
-    GenerateSantas(String), // group name
-}
-
-#[derive(Serialize, Deserialize, Debug)]
 pub struct User {
+    pub id: u32,
+    pub group_name: String,
+    pub admin_login: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PersonalId {
+    pub id: Option<u32>,
     pub login: String,
-    pub command: Commands,
 }
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct a {
-    pub name: String,
-    pub user_id: i32
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct zxc {
-    pub id: i32,
-    pub name: String,
-    pub closed: bool,
-}
-
