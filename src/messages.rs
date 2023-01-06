af71fd143b038b6b556338b9503ae1bc831b6093
sonix14 <roletskaya_sof@mail.ru>
2023-01-06 21:58:09 +0300

    add user to group

--- src/messages.rs
+++ src/messages.rs
@@ -1,4 +1,4 @@
-use crate::models::{User, Group};
+use crate::models::{User, Group, UserToGroup};
 use actix::Message;
 use diesel::QueryResult;
 use serde::Deserialize;
@@ -27,3 +27,10 @@ pub struct EnterGroup {
     pub user_id: i32
 }
 
+#[derive(Message, Deserialize)]
+#[rtype(result = "QueryResult<UserToGroup>")]
+pub struct MakeAdmin {
+    pub group_name: String,
+    pub user_id: i32,
+    pub future_admin_id: i32
+}
