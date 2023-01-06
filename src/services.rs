805c37cdc26c3f7145f9d0e7b61e61bd351df9a6
dasu <kirill12093@mail.ru>
2023-01-06 03:38:26 +0300

    fix

--- src/services.rs
+++ src/services.rs
@@ -18,7 +18,7 @@ pub async fn get_users(state: Data<AppState>) -> impl Responder {
     //отправляем сообщение актеру, так как у нас 5 потоков, то сможем отправлять 5 сообщений одновременно
     match db.send(GetUsers).await {
         Ok(Ok(info)) => {
-            HttpResponse::Ok().json("1")
+            HttpResponse::Ok().json(info)
         },
         Ok(Err(_)) => HttpResponse::BadRequest().json("No users found"),
         _ => HttpResponse::InternalServerError().json("Unable to retrieve users")
