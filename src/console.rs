4309d408f826923c717aaeebd8013b3cccb76a28
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-01-07 22:39:16 +0300

    adding new features to client

--- src/console.rs
+++
@@ -1,9 +0,0 @@
-use std::{io};
-
-pub fn get_data(data: &mut String) -> Result<usize, &str> {
-    let current_length = io::stdin().read_line(data).expect("Error: Input is invalid");
-    if current_length == 0 {
-        return Err("Programm was shutting down.");
-    }
-    Ok(current_length)
-}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,20 @@
-pub mod console;
-use console::get_data;
-pub mod structures;
-use structures::User;
-pub mod generator;
-use generator::get_urls_container;
+pub mod console_interaction;
+use console_interaction::{get_data, check_command, print_help, do_extra_console_command};
+pub mod urls_generator;
+use urls_generator::get_urls_container;
 use std::collections::HashMap;
 use reqwest::StatusCode;
-
-#[tokio::main]
-async fn print_command_result(url: String, data: User) -> Result<String, reqwest::Error> {
-    let response: String = reqwest::Client::new()
-        .post(url)
-        .json(&data)
-        .send()
-        .await?
-        .json()
-        .await?;
-    Ok(response)
-}
+pub mod server_interaction;
+use server_interaction::{get_command_result, User};
+use colored::Colorize;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
+    let mut instruction_should_be_shown = true;
     let urls_container:HashMap<String, String> = get_urls_container();
     loop {
         let mut user_login = String::new();
-        println!("Enter your login:");
+        println!("{}", format!("Enter your login:").yellow());
         if get_data(&mut user_login).is_err() {
             break;
         }
@@ -40,26 +30,31 @@ async fn main() -> Result<(), reqwest::Error> {
             current_id.replace(current_user);
         } else {
             let error_message: String = current_user_response.json().await?;
-            println!("{}", error_message);
+            println!("{}", format!("{}", error_message).purple().italic());
             continue;
         }
         let current_id = current_id.unwrap();
         loop {
+            if instruction_should_be_shown {
+                print_help();
+                instruction_should_be_shown = false;
+            }
             let mut command_args = String::new();
-            println!("Enter your command:");
+            println!("{}", format!("Enter your command:").yellow());
             if get_data(&mut command_args).is_err() {
                 break;
             }
             let command_args: Vec<_> = command_args.trim().split(" ").collect();
             let command: &str = command_args[0];
-            let amount_of_args: usize = command_args.len();
-
-
-            if (amount_of_args < 2 || amount_of_args > 3) ||  (amount_of_args == 2 && command == "assign") || (amount_of_args == 3 && command != "assign") {
-                println!("Wrong format of command!");
+            if check_command(command, &command_args.len()) {
+                println!("{}", format!("Wrong format of command!").purple().italic());
                 continue;
             }
 
+            if do_extra_console_command(command) {
+                continue;
+            }
+            
             let mut admin_name: String = user_login.to_string();
             if command == "assign" {
                 admin_name = command_args[2].to_string();
@@ -73,20 +68,21 @@ async fn main() -> Result<(), reqwest::Error> {
                 Some(url) => {
                     let needed_url  = url.clone();
                     tokio::task::spawn_blocking(|| {
-                        println!("{}", print_command_result(needed_url, current_command).unwrap());
+                        println!("{}", format!("{}", get_command_result(needed_url, current_command).unwrap()).purple().italic());
                     }).await.expect("Task panicked")
                 },
                 None => {
-                    println!("Wrong format of command!");
+                    println!("{}", format!("Wrong format of command!").purple().italic());
                     continue;
                 },
             }
         }
-        println!("You have been log out of profile. Would you like to log in to your profile?[Y/N]");
+        println!("{}", format!("You have been log out of profile. Would you like to log in to your profile?[Y/N]").yellow());
         let mut change_profile: String = String::new();
-        if get_data(&mut change_profile).is_err() || !(change_profile == "Y" || change_profile == "y") {
+        if get_data(&mut change_profile).is_err() || !(change_profile.trim() == "Y" || change_profile.trim() == "y") {
             break;
         }
+        instruction_should_be_shown = true;
     }
     Ok(())
 }
