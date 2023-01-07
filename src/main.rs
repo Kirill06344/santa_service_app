f5bec3d7e7b23dcd8bf4038392a5bd69fcb6f8e1
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-01-07 04:00:41 +0300

    fixing style

--- src/main.rs
+++ src/main.rs
@@ -22,16 +22,13 @@ async fn print_command_result(url: String, data: User) -> Result<String, reqwest
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let urls_container:HashMap<String, String> = get_urls_container();
-
     loop {
         let mut user_login = String::new();
         println!("Enter your login:");
         if get_data(&mut user_login).is_err() {
             break;
         }
-
         let user_login = user_login.trim();
-
         let current_user_response = reqwest::Client::new()
             .post("http://127.0.0.1:8080/get_login_id")
             .json(&user_login)
@@ -41,15 +38,12 @@ async fn main() -> Result<(), reqwest::Error> {
         if current_user_response.status() == StatusCode::OK {
             let current_user: i32 = current_user_response.json().await?;
             current_id.replace(current_user);
-        }
-        else {
+        } else {
             let error_message: String = current_user_response.json().await?;
             println!("{}", error_message);
             continue;
         }
-
         let current_id = current_id.unwrap();
-
         loop {
             let mut command_args = String::new();
             println!("Enter your command:");
@@ -58,17 +52,14 @@ async fn main() -> Result<(), reqwest::Error> {
             }
             let command_args: Vec<_> = command_args.trim().split(" ").collect();
             let command: &str = command_args[0];
-
             let amount_of_args: usize = command_args.len();
             if (amount_of_args == 3 && command != "assign")  || amount_of_args != 2 {
                 continue;
             }
-            
             let mut admin_name: String = user_login.to_string();
             if command == "assign" {
                 admin_name = command_args[2].to_string();
             }
-
             let current_command: User = User {
                 group_name: (command_args[1].to_string()),
                 user_id: (current_id),
@@ -76,29 +67,17 @@ async fn main() -> Result<(), reqwest::Error> {
             };
             match urls_container.get(command) {
                 Some(url) => {
-                    // //1-st bariant
-                    // let message: String = reqwest::Client::new()
-                    //     .post(url)
-                    //     .json(&current_command)
-                    //     .send()
-                    //     .await?
-                    //     .json()
-                    //     .await?;
-                    // println!("{}", message);
                     let needed_url  = url.clone();
-                                    //2-nd bariant
                     tokio::task::spawn_blocking(|| {
                         println!("{}", print_command_result(needed_url, current_command).unwrap());
-                    })
-                    .await
-                    .expect("Task panicked")
+                    }).await.expect("Task panicked")
                 },
                 None => continue,
             }
         }
         println!("You have been log out of profile. Would you like to log in to your profile?[Y/N]");
         let mut change_profile: String = String::new();
-        if !(get_data(&mut change_profile).is_ok() && (change_profile == "Y" || change_profile == "y")) {
+        if get_data(&mut change_profile).is_err() || !(change_profile == "Y" || change_profile == "y") {
             break;
         }
     }
