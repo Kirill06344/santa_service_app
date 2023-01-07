b922a0a43efc0c12a951677b7a812f8d5e773d97
anastasiagorbunova <an.gorbunovaa@gmail.com>
2023-01-08 02:52:05 +0300

    adding new struct InsertableSanta

--- src/insertables.rs
+++ src/insertables.rs
@@ -1,4 +1,5 @@
 use crate::schema::user_group;
+use crate::schema::santa;
 use diesel::Insertable;
 use serde::{Serialize, Deserialize};
 
@@ -8,4 +9,12 @@ pub struct InsertableUserGroup {
     pub user_id: i32,
     pub group_id: i32,
     pub is_admin: bool
+}
+
+#[derive(Insertable, Serialize, Deserialize, Clone)]
+#[diesel(table_name = santa)]
+pub struct InsertableSanta {
+    pub user_id: i32,
+    pub present_id: i32,
+    pub group_id: i32
 }
